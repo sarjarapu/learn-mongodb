19537b243fd305afc2eb24305d9a4a05ae714ff3
Shyam Arjarapu <shyam.arjarapu@gmail.com>
2016-10-05 22:49:46 -0500

    Adding sample code to connect to MongoDB

---
+++ csharp/01-ConnectToMongoDB/Program.cs
@@ -0,0 +1,45 @@
+﻿using MongoDB.Driver;
+using System;
+using System.Configuration;
+using System.Linq;
+
+namespace ConnectToMongoDB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var program = new Program();
+            program.Run();
+            
+            Console.WriteLine("Press ENTER to exit");
+            Console.ReadLine();
+        }
+
+        void Run()
+        {
+            var connStr = ConfigurationManager.AppSettings.Get("mongoDBConnStr");
+            try
+            {
+                Console.WriteLine("Connecting to MongoDB @ {0}", connStr);
+
+                var client = new MongoClient(connStr);
+                
+                Console.WriteLine("Listing all databases on server");
+                using (var cursor = client.ListDatabases())
+                {
+                    foreach (var doc in cursor.ToList())
+                    {
+                        Console.WriteLine("\t{0}", doc["name"]);
+                    }
+                }
+                Console.WriteLine("Congratulations! You made it through connecting to MongoDB");
+            }
+            catch (TimeoutException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            Console.WriteLine("");
+        }
+    }
+}
