d307e406e37ff428f6c26dabb633cccddb7e5552
Shyam Arjarapu <shyam.arjarapu@gmail.com>
2016-10-06 14:12:29 -0500

    Added sample code for insertMany documents

--- csharp/02-CreateDocument/Program.cs
+++ csharp/02-CreateDocument/Program.cs
@@ -44,8 +44,15 @@ namespace CreateDocument
                 var doc = coll.Find(new BsonDocument()).Limit(1).FirstOrDefault();
                 Console.WriteLine("  {0}\n", doc);
 
+                List<BsonDocument> bsonDocs = GetBsonDocuments();
+                coll.InsertMany(bsonDocs);
+
                 Console.WriteLine("");
-                Console.WriteLine("Congratulations! You made it through create documents in MongoDB");
+                Console.WriteLine("Creation of mulitple documents is completed. Let's read them all from database");
+                coll.Find(new BsonDocument()).ToList().ForEach(d => Console.WriteLine("  {0}", d));
+
+                Console.WriteLine("");
+                Console.WriteLine("Congratulations! You made it through creation of documents in MongoDB");
             }
             catch (TimeoutException e)
             {
@@ -67,6 +74,59 @@ namespace CreateDocument
                     } }
                 };
             return new BsonDocument(data);
+        }
+
+        private static List<BsonDocument> GetBsonDocuments()
+        {
+            var rand = new Random(100);
+            var cities = new [] {
+                new Tuple<string, string>("Newark", "NJ" ),
+                new Tuple<string, string>("New York", "NY"),
+                new Tuple<string, string>("Dallas", "TX"),
+                new Tuple<string, string>("Austin", "TX"),
+                new Tuple<string, string>("San Francisco", "CA"),
+                new Tuple<string, string>("Mountain View", "CA"),
+                new Tuple<string, string>("Seattle", "WA"),
+                new Tuple<string, string>("Salt Lake City", "UT")
+            };
+            var documents = new List<BsonDocument>();
+            for(var i = 0; i < 5; i++)
+            {
+                var city = cities[rand.Next(cities.Length)];
+                var likes = GetRandomLikes(rand);
+                // document has string keys with values as string/int/array of strings/subdocument
+                var data = new Dictionary<string, object>{
+                    { "name", $"F{i} N{i}" },
+                    { "linkedin", $"https://www.linkedin.com/in/fn{i}" },
+                    { "connections", rand.Next(10000) },
+                    { "likes" , likes },
+                    { "address" , new Dictionary<string, object>{
+                        { "city", city.Item1 }, {"state", city.Item2 }
+                    } }
+                };
+                documents.Add(new BsonDocument(data));
+            }
+            return documents;
+        }
+
+        private static string[] GetRandomLikes(Random rand)
+        {
+            var likes = new[] { ".net", "nodejs", "mongodb", "java", "oracle", "ruby", "python" };
+            var count = rand.Next(likes.Length + 1);
+            if (count > 0)
+            {
+                var selected = new string[count];
+                for (int i = 0; i < count; i++)
+                {
+                    var like = likes[rand.Next(likes.Length)];
+                    if (!selected.Contains(like))
+                    {
+                        selected[i] = like;
+                    }
+                }
+                return selected;
+            }
+            return null;
         }
     }
 }
