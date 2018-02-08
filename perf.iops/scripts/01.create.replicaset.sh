953df57c8d9ddeafc4235a23f4022ad372b53ba8
shyamarjarapu <shyamarjarapu@SHYAMARJARA2F91>
2018-02-08 08:20:27 -0600

    added permanent transparent huge pages disabled

--- perf.iops/scripts/01.create.replicaset.sh
+++ perf.iops/scripts/01.create.replicaset.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 cd ../ansible
 
-# create aws instances for the mongodb servers
-ansible-playbook create.instances.yml
+# # create aws instances for the mongodb servers
+# ansible-playbook create.instances.yml
 
-# Wait for 2/2 Status Checks
-sleep 300
+# # # Wait for 2/2 Status Checks
+# # sleep 300
 
 # configure the mongodb replicaset 
 ansible-playbook create.replicaset.yml
